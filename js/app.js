dba09e640a821cdb1c72aea23016ee3454cec36d
yeyemelodie01 <melodieyeye@gmail.com>
2023-11-14 16:23:42 +0100

    add function  page save.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
-import { getData, saveData } from "./modules/save.js";
+//import { getData, saveData} from "./modules/save.js";
+
+const linkNav = document.querySelectorAll("a");
+let title = document.getElementById("nav_page");
+
+for (const element of linkNav) {
+    element.addEventListener("click", function() {
+    localStorage.setItem("title", element.textContent); 
+    });
+}
+title.textContent = localStorage.getItem("title");
+ console.log(title);
+
 
 
 
